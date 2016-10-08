f68ea86c38e2b2dde14ef18b4f96e3ac8b784600
alessandrocssr <osque.an@gmail.com>
2016-10-08 11:05:52 -0300

    Add files via upload

---
+++ UntMain.pas
@@ -0,0 +1,35 @@
+unit UntMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  Data.Bind.GenData, Fmx.Bind.GenData, FMX.ListView.Types,
+  MultiDetailAppearanceU, Data.Bind.EngExt, Fmx.Bind.DBEngExt, System.Rtti,
+  System.Bindings.Outputs, Fmx.Bind.Editors, Data.Bind.Components, FMX.ListView,
+  Data.Bind.ObjectScope;
+
+type
+  TForm1 = class(TForm)
+    PrototypeBindSource1: TPrototypeBindSource;
+    ListView1: TListView;
+    BindingsList1: TBindingsList;
+    LinkListControlToField1: TLinkListControlToField;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+{$R *.NmXhdpiPh.fmx ANDROID}
+{$R *.SmXhdpiPh.fmx ANDROID}
+{$R *.LgXhdpiPh.fmx ANDROID}
+
+end.
